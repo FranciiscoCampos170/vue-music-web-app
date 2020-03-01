c4e2a53cb81092668ce7d7b65c649c1089190174
Francisco Campos <franciiscocampos170@gmail.com>
2020-03-01 12:10:57 +0100

    created prev and next methods

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,17 @@
           {{  current.title }} - <span>{{ current.artist}}</span>
         </h2>
         <div class="control">
-          <button>Prev</button>
+          <button @click="prev">Prev</button>
           <button v-if="!isPlaying" @click="play">Play</button>
           <button v-else @click="pause">Pause</button>
-          <button>Next</button>
+          <button @click="next">Next</button>
         </div>
       </section>
       <section class="playlist">
         <h3>The Playlist</h3>
-        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song' "></button>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
+          {{song.title}} - {{song.artist}}
+        </button>
       </section>
     </main>
   </div>
@@ -57,11 +59,34 @@ export default {
       }
 
       this.player.play();
+      this.player.addEventListener('ended', function() {
+        
+      }.bind(this));
       this.isPlaying = true;
     },
     pause () {
       this.player.pause();
       this.isPlaying = false;
+    },
+    prev () {
+      this.index--; 
+      if(this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+
+      this.current = this.songs[this.index];
+
+      this.play(this.current);
+    },
+    next () {
+      this.index++; 
+      if(this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+
+      this.current = this.songs[this.index];
+
+      this.play(this.current);
     }
   },
   created() {
