d3dab752e33cdefe67c2f57e8c78d92bf1a67377
Francisco Campos <franciiscocampos170@gmail.com>
2020-03-01 11:46:51 +0100

    added controls to play and pause the song

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,16 @@
         <h2 class="song-title">
           {{  current.title }} - <span>{{ current.artist}}</span>
         </h2>
+        <div class="control">
+          <button>Prev</button>
+          <button v-if="!isPlaying" @click="play">Play</button>
+          <button v-else @click="pause">Pause</button>
+          <button>Next</button>
+        </div>
+      </section>
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song' "></button>
       </section>
     </main>
   </div>
@@ -22,6 +32,7 @@ export default {
     return {
       current: {},
       index: 0,
+      isPlaying: false,
       songs: [
       {
         title: 'Spectre',
@@ -31,10 +42,26 @@ export default {
       {
         title: 'Faded',
         artist: 'Alan Walker',
-        src: require('./assets/alan-walker-spectre.mp3')
+        src: require('./assets/alan-walker-fade.mp3')
       }
       ],
       player: new Audio()
+    }
+  },
+  methods: {
+    play(song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+
+        this.player.src = this.current.src;
+      }
+
+      this.player.play();
+      this.isPlaying = true;
+    },
+    pause () {
+      this.player.pause();
+      this.isPlaying = false;
     }
   },
   created() {
